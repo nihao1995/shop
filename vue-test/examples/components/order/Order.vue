e3dfee256720b3094986f2513c050c4fc1bc215d
徐强 <1379577123@qq.com>
2019-09-28 09:25:15 +0800

    add ignore

--- vue-test/examples/components/order/Order.vue
+++ vue-test/examples/components/order/Order.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <Header msg_title="订单中心" msg_right="" type="1" @func="getMsgFormSon"></Header>
-        <van-tabs style="height: 100%;" v-model="active" animated swipeable>
+        <van-tabs v-model="active" animated swipeable>
             <van-tab title="全部">
-                <div class="box" v-for="(item,index) in list">
+                <div class="box" v-for="item in order_list">
                     <van-card style="background: #fff"
                             num="2"
                             price="2.00"
@@ -38,8 +38,10 @@
                 </div>
             </van-tab>
             <van-tab title="待付款">
-                <div class="box" v-for="(item,index) in list" v-if="item.type==1">
-                    <van-card style="background: #fff"
+                <template v-for="item in order_list">
+                    <template v-if="item.type==1">
+                        <div class="box">
+                            <van-card style="background: #fff"
                               num="2"
                               price="2.00"
                               desc="描述信息"
@@ -56,11 +58,15 @@
                             <van-button size="mini">付款</van-button>
                         </div>
                     </van-card>
-                </div>
+                        </div>
+                    </template>
+                </template>
             </van-tab>
             <van-tab title="待发货">
-                <div class="box" v-for="(item,index) in list" v-if="item.type==2">
-                    <van-card style="background: #fff"
+                <template v-for="item in order_list">
+                    <template v-if="item.type==2">
+                        <div class="box">
+                            <van-card style="background: #fff"
                               num="2"
                               price="2.00"
                               desc="描述信息"
@@ -76,11 +82,15 @@
                             <van-button size="mini">挑选服务</van-button>
                         </div>
                     </van-card>
-                </div>
+                        </div>
+                    </template>
+                </template>
             </van-tab>
-            <van-tab title="待收货" v-for="(item,index) in list" v-if="item.type==3">
-                <div class="box">
-                    <van-card style="background: #fff"
+            <van-tab title="待收货">
+                <template v-for="item in order_list">
+                    <template v-if="item.type==3">
+                        <div class="box">
+                            <van-card style="background: #fff"
                               num="2"
                               price="2.00"
                               desc="描述信息"
@@ -97,28 +107,34 @@
                             <van-button style="border-color: #FF5000;color: #FF5000" size="mini">确认收货</van-button>
                         </div>
                     </van-card>
-                </div>
+                        </div>
+                    </template>
+                </template>
             </van-tab>
-            <van-tab title="待评价" v-for="(item,index) in list" v-if="item.type==4">
-                <div class="box">
-                    <van-card style="background: #fff"
-                              num="2"
-                              price="2.00"
-                              desc="描述信息"
-                              :title= item.title
-                              thumb="https://img.yzcdn.cn/vant/t-thirt.jpg"
-                    >
-                        <div slot="tags">
-                            <van-tag plain type="danger">标签</van-tag>
-                            <van-tag plain type="danger">标签</van-tag>
+            <van-tab title="待评价">
+                <template  v-for="item in order_list">
+                    <template v-if="item.type==4">
+                        <div class="box">
+                            <van-card style="background: #fff"
+                                  num="2"
+                                  price="2.00"
+                                  desc="描述信息"
+                                  :title= item.title
+                                  thumb="https://img.yzcdn.cn/vant/t-thirt.jpg"
+                        >
+                            <div slot="tags">
+                                <van-tag plain type="danger">标签</van-tag>
+                                <van-tag plain type="danger">标签</van-tag>
+                            </div>
+                            <div slot="footer">
+                                <van-button size="mini">查看物流</van-button>
+                                <van-button size="mini">卖了换钱</van-button>
+                                <van-button size="mini">评价</van-button>
+                            </div>
+                        </van-card>
                         </div>
-                        <div slot="footer">
-                            <van-button size="mini">查看物流</van-button>
-                            <van-button size="mini">卖了换钱</van-button>
-                            <van-button size="mini">评价</van-button>
-                        </div>
-                    </van-card>
-                </div>
+                    </template>
+                </template>
             </van-tab>
         </van-tabs>
     </div>
@@ -134,15 +150,15 @@
             return {
                 msgFormSon:'',
                 active: 0,
-                list:[
+                order_list:[
                     {type:1,title:"待付款标题1"},
                     {type:2,title:"待发货标题1"},
-                    // {type:3,title:"待收货标题1"},
-                    // {type:4,title:"待评价标题1"},
-                    // {type:1,title:"待付款标题2"},
-                    // {type:2,title:"待发货标题2"},
-                    // {type:3,title:"待收货标题2"},
-                    // {type:4,title:"待评价标题2"},
+                    {type:3,title:"待收货标题1"},
+                    {type:4,title:"待评价标题1"},
+                    {type:1,title:"待付款标题2"},
+                    {type:2,title:"待发货标题2"},
+                    {type:3,title:"待收货标题2"},
+                    {type:4,title:"待评价标题2"},
                 ]
             };
         },
@@ -171,6 +187,7 @@
         height: auto;
         border-radius: 5px;
         background: #fff;
+        border: 1px solid #eee;
         box-sizing: border-box;
         padding: 5px 0;
         margin: 10px;
